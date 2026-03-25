12d217ff419bfa61c37a34821cc0614c6b5449e1
Breaveluv <nam.bule12@gmail.com>
2026-03-25 23:56:53 +0700

    add

---
+++ src/FLASHSALE/src/com/flashsale/Main.java
@@ -0,0 +1,12 @@
+package com.flashsale;
+
+import SS14.com.flashsale.FlashSaleApp;
+
+/**
+ * This class is deprecated. Please use com.flashsale.FlashSaleApp instead.
+ */
+public class Main {
+    public static void main(String[] args) {
+        FlashSaleApp.main(args);
+    }
+}
